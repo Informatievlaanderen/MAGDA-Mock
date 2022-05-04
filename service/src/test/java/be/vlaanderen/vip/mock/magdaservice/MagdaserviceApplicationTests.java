c5a4324d8c63f9673742a7a998516987eeb7c19b
Pascal Van Cauwenberghe <pvc@nayima.be>
2022-05-04 12:51:37 +0200

    Basic project structure with parent + service

---
+++ service/src/test/java/be/vlaanderen/vip/mock/magdaservice/MagdaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vlaanderen.vip.mock.magdaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagdaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
