ce7c33948f79f7a2ce50601fc4aef2ebcf270aa9
Wout Neirynck <wout.neirynck@d-n.be>
2022-11-07 09:37:34 +0100

    Added mockito

---
+++ interfaces/src/test/java/be/vlaanderen/vip/magda/client/MagdaConnectorImplTest.java
@@ -0,0 +1,14 @@
+package be.vlaanderen.vip.magda.client;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MagdaConnectorImplTest {
+
+	@Test
+	void returnsReply() {
+		
+	}
+}
