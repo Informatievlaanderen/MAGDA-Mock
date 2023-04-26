4e7a13a01d638f210b396009fbe4718ad0597888
Benjamin Barrett <benjamin.barret@mateco.eu>
2023-04-26 17:02:59 +0200

    remove commented code

--- magdamock/src/main/java/be/vlaanderen/vip/mock/magda/client/MagdaMockConnection.java
+++ magdamock/src/main/java/be/vlaanderen/vip/mock/magda/client/MagdaMockConnection.java
@@ -1,19 +1,12 @@
 package be.vlaanderen.vip.mock.magda.client;
 
-import be.vlaanderen.vip.magda.client.MagdaDocument;
-import be.vlaanderen.vip.magda.client.connection.MagdaConnection;
-import be.vlaanderen.vip.magda.client.security.DocumentSignatureVerifier;
-import be.vlaanderen.vip.magda.client.security.DocumentSigner;
-import be.vlaanderen.vip.magda.client.security.TwoWaySslProperties;
-import be.vlaanderen.vip.magda.exception.MagdaSendFailed;
-import be.vlaanderen.vip.magda.exception.TwoWaySslException;
-import be.vlaanderen.vip.mock.magda.client.simulators.*;
-import be.vlaanderen.vip.mock.magda.inventory.ResourceFinder;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 
-import java.util.Optional;
+import be.vlaanderen.vip.magda.client.MagdaDocument;
+import be.vlaanderen.vip.magda.client.connection.MagdaConnection;
+import be.vlaanderen.vip.magda.exception.MagdaSendFailed;
+import be.vlaanderen.vip.mock.magda.client.simulators.SOAPSimulator;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MagdaMockConnection implements MagdaConnection {
@@ -24,37 +17,6 @@ public class MagdaMockConnection implements MagdaConnection {
 
     public MagdaMockConnection(SOAPSimulator simulator) {
         this.simulator = simulator;
-    }
-    
-//    public MagdaMockConnection(ResourceFinder finder, TwoWaySslProperties requestVerifierConfig, TwoWaySslProperties responseSignerConfig) throws TwoWaySslException {
-//        Optional<DocumentSignatureVerifier> requestVerifier;
-//        Optional<DocumentSigner> responseSigner;
-//
-//        if(requestVerifierConfig != null && StringUtils.isNotEmpty(requestVerifierConfig.getKeyStoreLocation())) {
-//            requestVerifier = Optional.of(DocumentSignatureVerifier.fromJksStore(requestVerifierConfig));
-//        } else {
-//            requestVerifier = Optional.empty();
-//        }
-//
-//        if(responseSignerConfig != null && StringUtils.isNotEmpty(responseSignerConfig.getKeyStoreLocation())) {
-//            responseSigner = Optional.of(DocumentSigner.fromJksStore(responseSignerConfig));
-//        } else {
-//            responseSigner = Optional.empty();
-//        }
-//
-//        this.simulator = constructBuiltInSimulator(finder, requestVerifier, responseSigner);
-//    }
-
-    private static SOAPSimulator constructBuiltInSimulator(ResourceFinder finder, Optional<DocumentSignatureVerifier> requestVerifier, Optional<DocumentSigner> responseSigner) {
-        var builder = SOAPSimulatorBuilder.builder(finder)
-                                          .magdaMockSimulator();
-        
-        builder = requestVerifier.map(builder::requestVerifier)
-                                 .orElse(builder);
-        builder = responseSigner.map(builder::responseSigner)
-                                .orElse(builder);
-        
-        return builder.build();
     }
 
     @Override
