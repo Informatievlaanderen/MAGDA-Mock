9f0752c89f9454ec3b6e4a31d1cb5a8361454c04
Niels Desmet <niels.desmet2@vlaanderen.be>
2025-06-23 16:26:34 +0200

    feat: add a method to the GiveDmfaVoorWerknemerService, which provides the source Magda response along with the according DmfaAttest

--- interfaces/src/main/java/be/vlaanderen/vip/magda/client/domain/geefdmfavoorwerknemer/MagdaClientGiveDmfaVoorWerknemerService.java
+++ interfaces/src/main/java/be/vlaanderen/vip/magda/client/domain/geefdmfavoorwerknemer/MagdaClientGiveDmfaVoorWerknemerService.java
@@ -3,10 +3,7 @@ package be.vlaanderen.vip.magda.client.domain.geefdmfavoorwerknemer;
 import be.vlaanderen.vip.magda.client.MagdaClient;
 import be.vlaanderen.vip.magda.client.MagdaClientException;
 import be.vlaanderen.vip.magda.client.diensten.GeefDmfaVoorWerknemerRequest;
-import be.vlaanderen.vip.magda.client.diensten.GeefWerkrelatiesRequest;
-import be.vlaanderen.vip.magda.client.domain.giveworkrelations.GiveWorkRelationsService;
-import be.vlaanderen.vip.magda.client.domain.giveworkrelations.MagdaResponseWorkRelationsAdapter;
-import be.vlaanderen.vip.magda.client.domain.giveworkrelations.WorkRelations;
+import be.vlaanderen.vip.magda.client.domain.dto.WithMagdaResponse;
 
 public class MagdaClientGiveDmfaVoorWerknemerService implements GiveDmfaVoorWerknemerService {
 
@@ -23,5 +20,12 @@ public class MagdaClientGiveDmfaVoorWerknemerService implements GiveDmfaVoorWerk
     @Override
     public DmfaAttest getDmfaAttests(GeefDmfaVoorWerknemerRequest request) throws MagdaClientException {
         return adapter.adapt(service.send(request));
+    }
+
+    @Override
+    public WithMagdaResponse<DmfaAttest> getDmfaAttestsWithSourceResponse(GeefDmfaVoorWerknemerRequest request) throws MagdaClientException { // XXX test
+        var magdaResponseWrapper = service.send(request);
+
+        return new WithMagdaResponse<>(adapter.adapt(magdaResponseWrapper), magdaResponseWrapper.getResponse());
     }
 }
