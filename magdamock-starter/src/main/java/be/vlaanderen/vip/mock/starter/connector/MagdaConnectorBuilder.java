27f254118e71c452c8cd282d106a4e910491de52
Jan-Pieter Baert <janpieter.baert@vlaanderen.be>
2025-11-21 22:23:37 +0100

    fix: add MagdaConnector for mocked REST endpoints bean

--- magdamock-starter/src/main/java/be/vlaanderen/vip/mock/starter/connector/MagdaConnectorBuilder.java
+++ magdamock-starter/src/main/java/be/vlaanderen/vip/mock/starter/connector/MagdaConnectorBuilder.java
@@ -5,20 +5,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MagdaConnectorBuilder {
 
-    private MagdaConnectorBuilder() {}
-    
+    private MagdaConnectorBuilder() {
+    }
+
     public static MagdaConnectorBuilder builder() {
         return new MagdaConnectorBuilder();
     }
-    
+
     public MockMagdaConnectorBuilder mock() {
         log.debug("Creating a mock magda connector");
         return new MockMagdaConnectorBuilder();
     }
-    
+
+    public MockMagdaRestConnectorBuilder mockRest() {
+        log.debug("Creating a mock magda rest connector");
+        return new MockMagdaRestConnectorBuilder();
+    }
+
     public RemoteMagdaConnectionBuilder remote() {
         log.debug("Creating a remote magda connector");
         return new RemoteMagdaConnectionBuilder();
     }
-    
 }
