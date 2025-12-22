329c1522797c6ded2f02add7698885200b709b08
Jan-Pieter Baert <janpieter.baert@vlaanderen.be>
2025-12-22 16:18:29 +0100

    fix: organisationName typo in JSON for 2 cars

--- magdaconnector/src/main/java/be/vlaanderen/vip/magda/client/MagdaSignedConnection.java
+++ magdaconnector/src/main/java/be/vlaanderen/vip/magda/client/MagdaSignedConnection.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.wss4j.common.ext.WSSecurityException;
 import org.w3c.dom.Document;
 
+import java.net.URISyntaxException;
 import java.util.Optional;
 
 @Slf4j
