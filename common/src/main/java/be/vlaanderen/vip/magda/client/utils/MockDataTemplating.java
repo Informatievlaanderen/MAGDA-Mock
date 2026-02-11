ceb96a63d596e20eeb0b2310b15fe41ee6df4ade
Jan-Pieter Baert <janpieter.baert@vlaanderen.be>
2026-02-11 17:52:38 +0100

    feat: add handlebars dynamic date functions

---
+++ common/src/main/java/be/vlaanderen/vip/magda/client/utils/MockDataTemplating.java
@@ -0,0 +1,93 @@
+package be.vlaanderen.vip.magda.client.utils;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Template;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
+public class MockDataTemplating {
+
+    public static String processTemplatingValues(String response, OffsetDateTime now) {
+        try {
+            Handlebars handlebars = new Handlebars();
+            handlebars.registerHelper("formatDate", new DateFormatter());
+            handlebars.registerHelper("dateMath", new DateMathHelper());
+            handlebars.registerHelper("startOfQuarter", new StartQuarterHelper());
+            handlebars.registerHelper("endOfQuarter", new EndQuarterHelper());
+            Template template = handlebars.compileInline(response);
+            HashMap<String, Object> data = new HashMap<>();
+            data.put("now", now);
+            return template.apply(data);
+        } catch (IOException e) {
+            return response;
+        }
+    }
+
+    private static ChronoUnit parseUnit(String u) {
+        return switch (u.toLowerCase()) {
+            case "y" -> ChronoUnit.YEARS;
+            case "m" -> ChronoUnit.MONTHS;
+            case "h" -> ChronoUnit.HOURS;
+            case "s" -> ChronoUnit.SECONDS;
+            default -> ChronoUnit.DAYS;
+        };
+    }
+
+    public static class DateFormatter implements Helper<OffsetDateTime> {
+        @Override
+        public Object apply(OffsetDateTime date, Options options) {
+            String format = options.param(0, "yyyy-MM-dd");
+            return DateTimeFormatter.ofPattern(format).format(date);
+        }
+    }
+
+    public static class DateMathHelper implements Helper<OffsetDateTime> {
+        @Override
+        public Object apply(OffsetDateTime date, Options options) {
+            String addition = options.param(0);
+            Pattern dateTimeDiffPattern = Pattern.compile("([-+][0-9]*)?([a-z]+)", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = dateTimeDiffPattern.matcher(addition);
+            if (!matcher.find())
+                return date;
+            String chronoUnit = matcher.group(2).toLowerCase(Locale.ROOT);
+            int amountToAdd = Integer.parseInt(matcher.group(1));
+            return date.plus(amountToAdd, parseUnit(chronoUnit));
+        }
+    }
+
+    public static class StartQuarterHelper implements Helper<OffsetDateTime> {
+        @Override
+        public Object apply(OffsetDateTime date, Options options) {
+            OffsetDateTime newTime = date.minusDays(0);
+            int month = Math.floorDivExact(newTime.getMonthValue() - 1, 3) * 3 + 1;
+            int day = 1;
+            newTime = newTime.withMonth(month);
+            newTime = newTime.withDayOfMonth(day);
+            return newTime;
+        }
+    }
+
+    public static class EndQuarterHelper implements Helper<OffsetDateTime> {
+        @Override
+        public Object apply(OffsetDateTime date, Options options) {
+            OffsetDateTime newTime = date.plusMonths(3);
+            int month = Math.floorDivExact(newTime.getMonthValue() - 1, 3) * 3 + 1;
+            int day = 1;
+            newTime = newTime.withMonth(month);
+            newTime = newTime.withDayOfMonth(day);
+            newTime = newTime.minusDays(1);
+            return newTime;
+        }
+    }
+}
---
+++ common/src/test/java/be/vlaanderen/vip/magda/client/utils/MockDataTemplatingTests.java
@@ -0,0 +1,57 @@
+package be.vlaanderen.vip.magda.client.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MockDataTemplatingTests {
+    private final OffsetDateTime dateTime;
+
+    private MockDataTemplatingTests(){
+        this.dateTime = OffsetDateTime.of(1582, 10, 11, 0, 0, 0, 0, ZoneOffset.ofHours(0));
+    }
+
+    @Test
+    public void templatingShouldNotChangeStaticString(){
+        String staticString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nibh.";
+        String templatedString = MockDataTemplating.processTemplatingValues(staticString, OffsetDateTime.now());
+        assertEquals(staticString, templatedString);
+    }
+
+    @Test
+    public void templateingShouldFormatDate(){
+        String staticString = "Today is {{formatDate now}}";
+        String templatedString = MockDataTemplating.processTemplatingValues(staticString, dateTime);
+        assertEquals("Today is 1582-10-11", templatedString);
+    }
+
+    @Test
+    public void testTemplatingForDays(){
+        String templatedDateString = MockDataTemplating.processTemplatingValues("{{formatDate (dateMath now '-10d')}}", dateTime);
+        assertEquals("1582-10-01", templatedDateString);
+        templatedDateString = MockDataTemplating.processTemplatingValues("{{formatDate (dateMath now '-102d')}}", dateTime);
+        assertEquals("1582-07-01", templatedDateString);
+    }
+
+    @Test
+    public void testTemplatingForStartOfQuarter(){
+        String templatedDateString = MockDataTemplating.processTemplatingValues("{{formatDate (startOfQuarter now)}}", dateTime);
+        assertEquals("1582-10-01", templatedDateString);
+    }
+
+    @Test
+    public void testTemplatingForEndOfQuarter(){
+        String templatedDateString = MockDataTemplating.processTemplatingValues("{{formatDate (endOfQuarter now)}}", dateTime);
+        assertEquals("1582-12-31", templatedDateString);
+    }
+
+
+    @Test
+    public void testTemplatingForCombination(){
+        String templatedDateString = MockDataTemplating.processTemplatingValues("{{formatDate (endOfQuarter (dateMath now '-102d'))}}", dateTime);
+        assertEquals("1582-09-30", templatedDateString);
+    }
+}
