98510c55abf258ecb765ff7bb9e2461b00d34456
Jan-Pieter Baert <janpieter.baert@vlaanderen.be>
2026-02-16 14:39:01 +0100

    chore: move handling of templating to wiremock integrated handlebars

--- magdamock-rest/src/main/java/be/vlaanderen/vip/mock/magda/client/MagdaMockRestConnection.java
+++ magdamock-rest/src/main/java/be/vlaanderen/vip/mock/magda/client/MagdaMockRestConnection.java
@@ -10,11 +10,19 @@ import be.vlaanderen.vip.mock.magda.config.WireMockData;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.common.Urls;
 import com.github.tomakehurst.wiremock.direct.DirectCallHttpServer;
+import com.github.tomakehurst.wiremock.http.ContentTypeHeader;
+import com.github.tomakehurst.wiremock.http.Cookie;
+import com.github.tomakehurst.wiremock.http.FormParameter;
+import com.github.tomakehurst.wiremock.http.HttpHeader;
+import com.github.tomakehurst.wiremock.http.HttpHeaders;
 import com.github.tomakehurst.wiremock.http.ImmutableRequest;
+import com.github.tomakehurst.wiremock.http.QueryParameter;
 import com.github.tomakehurst.wiremock.http.Request;
 import com.github.tomakehurst.wiremock.http.RequestMethod;
 import com.github.tomakehurst.wiremock.http.Response;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.tuple.Pair;
@@ -23,14 +31,15 @@ import org.w3c.dom.Document;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -81,31 +90,162 @@ public class MagdaMockRestConnection implements MagdaConnection {
     public Pair<JsonNode, Integer> sendRestRequest(String path, String query, String method, String requestBody, String dateHeader) {
         List<String> parts = new ArrayList<>();
         parts.add(wireMockServer.url(path));
-        if (!query.isEmpty()){
+        if (!query.isEmpty()) {
             parts.add(query);
         }
         String url = String.join("?", parts);
         try {
-            Request mockRequest = new ImmutableRequest.Builder()
-                    .withAbsoluteUrl(url)
-                    .withMethod(RequestMethod.fromString(method))
-                    .build();
-            Response response = internalWiremockHttpServer.stubRequest(mockRequest);
-
+            Response response = internalWiremockHttpServer.stubRequest(createInternalWiremockRequest(url, method, dateHeader));
             if (response.getStatus() == 404) {
                 return Pair.of(null, 404);
             }
-            LocalDate date;
-            try {
-                date = LocalDate.parse(dateHeader, DateTimeFormatter.RFC_1123_DATE_TIME);
-            } catch (Exception ex) {
-                date = LocalDate.now();
-            }
             String responseBody = response.getBodyAsString();
-            responseBody = MockDataTemplating.processTemplatingValues(responseBody, date);
             return Pair.of(mapper.readTree(responseBody), response.getStatus());
         } catch (IOException e) {
             throw new MagdaMockRestException("Error simulating REST call", e.getCause());
         }
+    }
+
+    // As there need to be certain parameters filled in to avoid wiremock throwing nullpointers while templating, we create the request ourselves
+    private Request createInternalWiremockRequest(String url, String method, String dateHeader) {
+        if (dateHeader == null) {
+            dateHeader = "";
+        }
+        HttpHeaders httpHeaders = new HttpHeaders(new HttpHeader("Date", dateHeader));
+        return new Request() {
+            @Override
+            public String getUrl() {
+                return Urls.getPathAndQuery(url);
+            }
+
+            @Override
+            public String getAbsoluteUrl() {
+                return url;
+            }
+
+            @Override
+            public RequestMethod getMethod() {
+                return RequestMethod.fromString(method);
+            }
+
+            @Override
+            public String getScheme() {
+                return "";
+            }
+
+            @Override
+            public String getHost() {
+                return "";
+            }
+
+            @Override
+            public int getPort() {
+                return wireMockServer.port();
+            }
+
+            @Override
+            public String getClientIp() {
+                return "";
+            }
+
+            @Override
+            public String getHeader(String key) {
+                List<String> values = header(key).getValues();
+                if (values.isEmpty()) {
+                    return "";
+                }
+                return values.getFirst();
+            }
+
+            @Override
+            public HttpHeader header(String key) {
+                return getHeaders().getHeader(key);
+            }
+
+            @Override
+            public ContentTypeHeader contentTypeHeader() {
+                return new ContentTypeHeader("application/json");
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                return httpHeaders;
+            }
+
+            @Override
+            public boolean containsHeader(String key) {
+                return !getHeader(key).isEmpty();
+            }
+
+            @Override
+            public Set<String> getAllHeaderKeys() {
+                return getHeaders().keys();
+            }
+
+            @Override
+            public QueryParameter queryParameter(String key) {
+                return null;
+            }
+
+            @Override
+            public FormParameter formParameter(String key) {
+                return null;
+            }
+
+            @Override
+            public Map<String, FormParameter> formParameters() {
+                return Map.of();
+            }
+
+            @Override
+            public Map<String, Cookie> getCookies() {
+                return Map.of();
+            }
+
+            @Override
+            public byte[] getBody() {
+                return new byte[0];
+            }
+
+            @Override
+            public String getBodyAsString() {
+                return "";
+            }
+
+            @Override
+            public String getBodyAsBase64() {
+                return "";
+            }
+
+            @Override
+            public boolean isMultipart() {
+                return false;
+            }
+
+            @Override
+            public Collection<Part> getParts() {
+                return List.of();
+            }
+
+            @Override
+            public Part getPart(String name) {
+                return null;
+            }
+
+            @Override
+            public boolean isBrowserProxyRequest() {
+                return false;
+            }
+
+            @Override
+            public Optional<Request> getOriginalRequest() {
+                return Optional.empty();
+            }
+
+            @Override
+            public String getProtocol() {
+                return "";
+            }
+        };
     }
 }
--- magdamock-rest/src/main/java/be/vlaanderen/vip/mock/magda/config/EmbeddedWireMockBuilder.java
+++ magdamock-rest/src/main/java/be/vlaanderen/vip/mock/magda/config/EmbeddedWireMockBuilder.java
@@ -1,14 +1,25 @@
 package be.vlaanderen.vip.mock.magda.config;
 
+import be.vlaanderen.vip.magda.client.utils.MockDataTemplating;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.direct.DirectCallHttpServerFactory;
+import com.github.tomakehurst.wiremock.extension.TemplateHelperProviderExtension;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.util.FileCopyUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmbeddedWireMockBuilder {
     
@@ -53,8 +64,99 @@ public class EmbeddedWireMockBuilder {
         WireMockConfiguration config = WireMockConfiguration.wireMockConfig()
                 .port(wiremockPort)
                 .httpServerFactory(factory)
+                .globalTemplating(true)
+                .extensions(getTemplateHelperExtensions())
                 .usingFilesUnderDirectory(fileSource);
 
         return new WireMockData(new WireMockServer(config), factory);
+    }
+
+    private static TemplateHelperProviderExtension getTemplateHelperExtensions() {
+        return new TemplateHelperProviderExtension() {
+            @Override
+            public String getName() {
+                return "custom-helpers";
+            }
+
+            @Override
+            public Map<String, Helper<?>> provideTemplateHelpers() {
+                return Map.of("parseDate", new DateParseHelper(),
+                        "formatDate", new DateFormatter(),
+                        "dateMath", new DateMathHelper(),
+                        "startOfQuarter", new StartQuarterHelper(),
+                        "endOfQuarter", new EndQuarterHelper());
+            }
+        };
+    }
+
+
+    private static ChronoUnit parseUnit(String u) {
+        return switch (u.toLowerCase()) {
+            case "y" -> ChronoUnit.YEARS;
+            case "m" -> ChronoUnit.MONTHS;
+            case "h" -> ChronoUnit.HOURS;
+            case "s" -> ChronoUnit.SECONDS;
+            default -> ChronoUnit.DAYS;
+        };
+    }
+
+    private static class DateParseHelper implements Helper<Object> {
+        @Override
+        public Object apply(Object toParse, Options options) {
+            LocalDate date;
+            try {
+                date = LocalDate.parse(toParse.toString(), DateTimeFormatter.RFC_1123_DATE_TIME);
+            } catch (Exception ex) {
+                date = LocalDate.now();
+            }
+            return date;
+        }
+    }
+
+    private static class DateFormatter implements Helper<LocalDate> {
+        @Override
+        public Object apply(LocalDate date, Options options) {
+            String format = options.param(0, "yyyy-MM-dd");
+            return DateTimeFormatter.ofPattern(format).format(date);
+        }
+    }
+
+    private static class DateMathHelper implements Helper<LocalDate> {
+        @Override
+        public Object apply(LocalDate date, Options options) {
+            String addition = options.param(0);
+            Pattern dateTimeDiffPattern = Pattern.compile("([-+][0-9]*)?([a-z]+)", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = dateTimeDiffPattern.matcher(addition);
+            if (!matcher.find())
+                return date;
+            String chronoUnit = matcher.group(2).toLowerCase(Locale.ROOT);
+            int amountToAdd = Integer.parseInt(matcher.group(1));
+            return date.plus(amountToAdd, parseUnit(chronoUnit));
+        }
+    }
+
+    private static class StartQuarterHelper implements Helper<LocalDate> {
+        @Override
+        public Object apply(LocalDate date, Options options) {
+            LocalDate newTime = date.minusDays(0);
+            int month = Math.floorDivExact(newTime.getMonthValue() - 1, 3) * 3 + 1;
+            int day = 1;
+            newTime = newTime.withMonth(month);
+            newTime = newTime.withDayOfMonth(day);
+            return newTime;
+        }
+    }
+
+    private static class EndQuarterHelper implements Helper<LocalDate> {
+        @Override
+        public Object apply(LocalDate date, Options options) {
+            LocalDate newTime = date.plusMonths(3);
+            int month = Math.floorDivExact(newTime.getMonthValue() - 1, 3) * 3 + 1;
+            int day = 1;
+            newTime = newTime.withMonth(month);
+            newTime = newTime.withDayOfMonth(day);
+            newTime = newTime.minusDays(1);
+            return newTime;
+        }
     }
 }
