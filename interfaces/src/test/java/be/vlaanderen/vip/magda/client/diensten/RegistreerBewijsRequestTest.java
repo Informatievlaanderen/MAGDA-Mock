e21cb02444c775cdf9696a29d3cd1c7e64dc3b57
Niels Desmet <niels.desmet2@vlaanderen.be>
2026-03-16 11:45:37 +0100

    fix: adjust the element ordering in RegistreerBewijsRequests

--- interfaces/src/test/java/be/vlaanderen/vip/magda/client/diensten/RegistreerBewijsRequestTest.java
+++ interfaces/src/test/java/be/vlaanderen/vip/magda/client/diensten/RegistreerBewijsRequestTest.java
@@ -125,14 +125,6 @@ class RegistreerBewijsRequestTest {
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/DetailOnderwerp/Code"), is(equalTo("detailOnderwerp code"))),
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Vervalperiode"), is(equalTo("vervalperiode"))),
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/UrenVolwassenenonderwijs"), is(equalTo("123"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[1]/Instantierol/Naam"), is(equalTo("alternatieveInstantie 1 instantierol naam"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[1]/Instantie/Naam"), is(equalTo("alternatieveInstantie 1 instantie naam"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[2]/Instantierol/Naam"), is(equalTo("alternatieveInstantie 2 instantierol naam"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[2]/Instantie/Naam"), is(equalTo("alternatieveInstantie 2 instantie naam"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[1]/Naam"), is(equalTo("bijkomendeInformatie 1 naam"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[1]/Inhoud"), is(equalTo("bijkomendeInformatie 1 inhoud"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[2]/Naam"), is(equalTo("bijkomendeInformatie 2 naam"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[2]/Inhoud"), is(equalTo("bijkomendeInformatie 2 inhoud"))),
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Beroepskwalificaties/Beroepskwalificatie[1]/Naam"), is(equalTo("beroepskwalificatie 1 naam"))),
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Beroepskwalificaties/Beroepskwalificatie[1]/Code"), is(equalTo("beroepskwalificatie 1 code"))),
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Beroepskwalificaties/Beroepskwalificatie[2]/Naam"), is(equalTo("beroepskwalificatie 2 naam"))),
@@ -140,7 +132,15 @@ class RegistreerBewijsRequestTest {
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Deelkwalificaties/Deelkwalificatie[1]/Naam"), is(equalTo("deelkwalificatie 1 naam"))),
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Deelkwalificaties/Deelkwalificatie[1]/Code"), is(equalTo("deelkwalificatie 1 code"))),
                     () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Deelkwalificaties/Deelkwalificatie[2]/Naam"), is(equalTo("deelkwalificatie 2 naam"))),
-                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Deelkwalificaties/Deelkwalificatie[2]/Code"), is(equalTo("deelkwalificatie 2 code")))
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/Deelkwalificaties/Deelkwalificatie[2]/Code"), is(equalTo("deelkwalificatie 2 code"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[1]/Instantierol/Naam"), is(equalTo("alternatieveInstantie 1 instantierol naam"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[1]/Instantie/Naam"), is(equalTo("alternatieveInstantie 1 instantie naam"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[2]/Instantierol/Naam"), is(equalTo("alternatieveInstantie 2 instantierol naam"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/AlternatieveInstanties/AlternatieveInstantie[2]/Instantie/Naam"), is(equalTo("alternatieveInstantie 2 instantie naam"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[1]/Naam"), is(equalTo("bijkomendeInformatie 1 naam"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[1]/Inhoud"), is(equalTo("bijkomendeInformatie 1 inhoud"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[2]/Naam"), is(equalTo("bijkomendeInformatie 2 naam"))),
+                    () -> assertThat(request.getValue("//Bewijsregistratie/Bewijs/BijkomendeInformaties/BijkomendeInformatie[2]/Inhoud"), is(equalTo("bijkomendeInformatie 2 inhoud")))
             );
         }
 
